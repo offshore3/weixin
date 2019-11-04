30f3656f48fcb7028b3096b470df89c4e3c31a0b
jia.liu <jia.liu@dudesolutions.com>
2019-11-04 21:49:22 +0800

    Add authorization of wx.

--- Mini_Program/Code/Express_mini_program/pages/my/my.js
+++ Mini_Program/Code/Express_mini_program/pages/my/my.js
@@ -5,27 +5,56 @@ Page({
    * Page initial data
    */
   data: {
-    src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg',
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isGetUserInfo:false,
+    user:{
+      profilePhoto:'',
+      name:'',
+      phone:''
+    }
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    var that = this;
+
     wx.setNavigationBarTitle({
       title: '我的'
+    });
+
+    wx.getSetting({
+      success: function(res){
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称          
+          wx.getUserInfo({
+            success: function(res) {
+              that.setData({
+                isGetUserInfo: true,
+                user:{
+                  profilePhoto:res.userInfo.avatarUrl,
+                  name:res.userInfo.nickName,
+                  phone:'158****9170'
+                }
+              });
+            }
+          })
+        }
+      }
     })
   },
 
   gotoSenderInfo:function(){
     wx.navigateTo({
-      url: '../address/address-list?type=1',
+      url: '../address/list/address-list?type=1',
     })
   },
 
   gotoRecipientInfo: function () {
     wx.navigateTo({
-      url: '../address/address-list?type=2',
+      url: '../address/list/address-list?type=2',
     })
   },
 
@@ -33,5 +62,20 @@ Page({
     wx.navigateTo({
       url: '../myDiscountCoupon/list',
     })
+  },
+
+  bindGetUserInfo: function (e) {
+    if(e.detail.errMsg == 'getUserInfo:ok'){
+      var that = this;
+    
+      that.setData({
+        isGetUserInfo: true,
+        user:{
+          profilePhoto:e.detail.userInfo.avatarUrl,
+          name:e.detail.userInfo.nickName,
+          phone:'158****9170'
+        }
+      });
+    }
   }
 })
